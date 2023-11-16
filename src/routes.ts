ac5ce8c8cfde45cbd749507e979117c991b5ab9d
kasilianaoliveira <kasilianaoliveira@gmail.com>
2023-11-16 14:16:46 -0300

    Update routes after changes

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import multer from'multer';
+
 import { CreateUserController } from './controllers/user/createUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
@@ -12,8 +14,14 @@ import { DeletePointController } from './controllers/point/deletePointController
 import { DeleteUserController } from './controllers/user/deleteUserController';
 import { UpdateUserController } from './controllers/user/updateUserController';
 import { FilterPointsController } from './controllers/point/filterPointsController';
+import { listPlacesController } from './controllers/places/listPlacesController';
+import { UpdatePointController } from './controllers/point/updatePointController';
+import { UpdateItemController } from './controllers/item/updateItemsController';
+
+import uploadConfig from './config/multer';
 
 const router = Router();
+const upload = multer(uploadConfig)
 
 //USERS -> ok
 router.post('/users', new CreateUserController().handle);
@@ -24,15 +32,20 @@ router.put('/user/:id', new UpdateUserController().handle);
 
 //ITEMS -> ok
 router.get('/items', new ItemsListController().handle);
-
+router.put('/items/:id', new UpdateItemController().handle);
 //POINT
-router.post('/point', new CreatePointController().handle);
-router.get('/point',  new ListPointController().handle);
+router.post('/point', isAuthenticated, upload.single('image'), new CreatePointController().handle);
+router.get('/points',  new ListPointController().handle);
 router.get('/point/:id', new ListPointUserController().handle);
 router.delete('/point/:id', new DeletePointController().handle);
-router.get('/point/filter', new FilterPointsController().handle)
-
+router.get('/pointFilter', new FilterPointsController().handle)
+router.put('/point/:id', upload.single('image'), new UpdatePointController().handle)
 //Neighborhood
 router.get('/neighborhood', new ListNeighborhoodController().handle);
+
+
+//reciclagem
+router.get('/api/places', new listPlacesController().handle)
+
 
 export { router }
