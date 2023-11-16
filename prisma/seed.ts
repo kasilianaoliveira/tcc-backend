7e4e79dca48276c09fc75305aefc6db93cc3d76f
kasilianaoliveira <kasilianaoliveira@gmail.com>
2023-11-16 14:07:28 -0300

    Update the schema and migration

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,12 +3,17 @@ import { prismaClient } from "../src/prisma";
 
 async function main() {
   const itemsData = [
-    { title: 'Lâmpadas', image: 'lampadas.svg' },
-    { title: 'Pilhas e Baterias', image: 'baterias.svg' },
-    { title: 'Papéis e Papelão', image: 'papeis-papelao.svg' },
-    { title: 'Resíduos Eletrônicos', image: 'eletronicos.svg' },
-    { title: 'Resíduos Orgânicos', image: 'organicos.svg' },
-    { title: 'Óleo de Cozinha', image: 'oleo.svg' },
+    { title: 'Lâmpadas', image: 'lampadas.jpg' },
+    { title: 'Pilhas e Baterias', image: 'baterias.jpg' },
+    { title: 'Plástico', image: 'plastico.jpg' },
+
+    { title: 'Vidros', image: 'vidro.jpg' },
+    { title: 'Metais', image: 'metais.jpg' },
+    { title: 'Papéis e Papelão', image: 'papeis-papelao.jpg' },
+
+    { title: 'Resíduos Eletrônicos', image: 'eletronicos.jpg' },
+    { title: 'Resíduos Orgânicos', image: 'organicos.jpg' },
+    { title: 'Óleo de Cozinha', image: 'oleo.jpg' },
   ];
 
   for (const item of itemsData) {
